66d55dd811591390e46079bdb33885c65df23ada
Narzog <eqbui@mit.edu>
2023-11-12 16:30:15 -0500

    updated
    updated

--- untitled.m
+++ untitled.m
@@ -1,1 +1,2 @@
+wut
 (x*Power[e,-b * Divide[Power[x,2],2]]*Power[e,-a*Power[x,3]](-D[\(40)x*Power[e,-b * Divide[Power[x,2],2]]*Power[e,-a*Power[x,3]]\(41) ,{x,2}]+ Power[x,4]*Power[e,-b * Divide[Power[x,2],2]]*Power[e,-a*Power[x,3]]))
